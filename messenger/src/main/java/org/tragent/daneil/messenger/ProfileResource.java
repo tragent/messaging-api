55b07d58b8d3a68ab8f3dedb9e3dbe5f7bd12c4e
Daniel Carlson <dancarl758@gmail.com>
2016-12-10 12:31:02 +0100

    Added Profile resource

---
+++ messenger/src/main/java/org/tragent/daneil/messenger/ProfileResource.java
@@ -0,0 +1,61 @@
+package org.tragent.daneil.messenger;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.tragent.daneil.messenger.model.Messages;
+import org.tragent.daneil.messenger.model.Profile;
+import org.tragent.daneil.messenger.service.ProfileService;
+
+@Path("/profiles")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProfileResource {
+
+	ProfileService profileService = new ProfileService();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Profile> getProfiles(){
+		return profileService.getAllProfiles();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Profile addProfile(Profile profile){
+		return profileService.addProfile(profile);
+	}
+	
+	@GET
+	@Path("/{profileName}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Profile getProfile(@PathParam("messageId") String profileName){
+		return profileService.getProfile(profileName);
+	}
+	
+	@PUT
+	@Path("/{profileName}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Profile updateProfile(@PathParam("profileName") String profileName, Profile profile){
+		profile.setProfileName(profileName);
+		return profileService.updateProfile(profile);
+	}
+	
+	@DELETE
+	@Path("/{profileName}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteProfile(@PathParam("profileName") String profileName){
+		profileService.removeProfile(profileName);
+	}
+}
--- messenger/src/main/java/org/tragent/daneil/messenger/database/DatabaseClass.java
+++ messenger/src/main/java/org/tragent/daneil/messenger/database/DatabaseClass.java
@@ -9,13 +9,13 @@ import org.tragent.daneil.messenger.model.Profile;
 public class DatabaseClass {
 
 	private static Map<Long, Messages> messages = new HashMap<>();
-	private static Map<Long, Profile> profiles = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
 	
 	public static Map<Long, Messages> getMessages(){
 		return messages;
 	}
 	
-	public static Map<Long, Profile> getProfiles() {
+	public static Map<String, Profile> getProfiles() {
 		return profiles;
 	}
 }
